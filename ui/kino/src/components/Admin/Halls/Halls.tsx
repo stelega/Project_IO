4356c2bacc7ba24021da821c58d6579b6004901e
piter984 <51178267+piter984@users.noreply.github.com>
2020-05-17 04:51:09 +0200

    admin menu (#16)

---
+++ ui/kino/src/components/Admin/Halls/Halls.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Halls = () => {
+  return <div>Sale kinowe</div>;
+};
+
+export default Halls;
--- ui/kino/src/components/Admin/MainPage.tsx
+++ ui/kino/src/components/Admin/MainPage.tsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopBar from '../TopBar';
+import {
+  MenuList,
+  MenuItem as mItem,
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core';
+import styled from 'styled-components';
+import Employees from './Employees/Employees';
+import Halls from './Halls/Halls';
+import Films from './Films/Films';
+
+const Container = styled.div`
+  display: flex;
+`;
+const Menu = styled.div`
+  width: 10vw;
+  height: 100vh;
+  border-right: solid black 1px;
+`;
+const MenuItem = styled(mItem)`
+  font-weight: bold;
+`;
+const theme = createMuiTheme({
+  overrides: {
+    MuiMenuItem: {
+      root: {
+        display: 'block',
+        textAlign: 'center',
+        '&$selected': {
+          backgroundColor: '#E5F9F4',
+          color: '#00C896',
+        },
+      },
+    },
+  },
+});
+
+const options = ['Filmy', 'Sale', 'Pracownicy'];
 
 const AdminMainPage = () => {
+  const [optionsState, setOptionsState] = useState('Filmy');
+  const handleClick = (value: string) => {
+    setOptionsState(value);
+  };
+
   return (
     <>
       <TopBar name={'Jan'} surname={'Kowalski'} />
-      Admin Page
+      <Container>
+        <ThemeProvider theme={theme}>
+          <Menu>
+            <MenuList>
+              {options.map((option) => (
+                <MenuItem
+                  key={option}
+                  selected={option === optionsState}
+                  onClick={() => handleClick(option)}
+                  value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </MenuList>
+          </Menu>
+        </ThemeProvider>
+        {optionsState === 'Filmy' ? (
+          <Films></Films>
+        ) : optionsState === 'Pracownicy' ? (
+          <Employees></Employees>
+        ) : (
+          <Halls></Halls>
+        )}
+      </Container>
     </>
   );
 };
