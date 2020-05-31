1cea962c142bcb9be189951489549f961d228836
Piotr Ksel <piotrek_ksel@wp.pl>
2020-06-01 00:08:20 +0200

    dodanie kolorów i refactoring

--- ui/kino/src/components/Admin/Halls/tableComponents/HallsTableBody.tsx
+++ ui/kino/src/components/Admin/Halls/tableComponents/HallsTableBody.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import {IconButton, TableBody, TableCell, TableRow} from '@material-ui/core';
+import { IconButton, TableBody, TableCell, TableRow } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
-import {Hall} from '../../../../models/Hall';
+import { Hall } from '../../../../models/Hall';
+import styled from 'styled-components';
+
+const Text = styled.div`
+  font-weight: bold;
+`;
+const Green = styled(Text)`
+  color: green;
+`;
+
+const Red = styled(Text)`
+  color: red;
+`;
 
 interface HallsTableBodyProps {
   halls: Hall[];
@@ -17,33 +29,33 @@ interface HallsTableBodyProps {
 }
 
 const HallsTableBody = (props: HallsTableBodyProps) => {
-  const {halls} = props;
+  const { halls } = props;
   return (
     <TableBody>
       {halls &&
-      props.halls.map((row) => {
-        return (
-          <TableRow hover tabIndex={-1} key={row.hallId}>
-            <TableCell align='center'>{row.name}</TableCell>
-            <TableCell align='center'>{row.numOfSeats}</TableCell>
-            <TableCell align='center'>{row.availability ? 'Tak' : 'Nie'}</TableCell>
-            <TableCell align='right'>
-              <IconButton
-                onClick={(event) =>
-                  props.handleEditClick(event, row.hallId)
-                }>
-                <EditIcon/>
-              </IconButton>
-              <IconButton
-                onClick={(event) =>
-                  props.handleDeleteClick(event, row.hallId)
-                }>
-                <DeleteIcon/>
-              </IconButton>
-            </TableCell>
-          </TableRow>
-        );
-      })}
+        props.halls.map((row) => {
+          return (
+            <TableRow hover tabIndex={-1} key={row.hallId}>
+              <TableCell align='center'>{row.name}</TableCell>
+              <TableCell align='center'>{row.numOfSeats}</TableCell>
+              <TableCell align='center'>
+                {row.availability ? <Green>Tak</Green> : <Red>Nie</Red>}
+              </TableCell>
+              <TableCell align='right'>
+                <IconButton
+                  onClick={(event) => props.handleEditClick(event, row.hallId)}>
+                  <EditIcon />
+                </IconButton>
+                <IconButton
+                  onClick={(event) =>
+                    props.handleDeleteClick(event, row.hallId)
+                  }>
+                  <DeleteIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          );
+        })}
     </TableBody>
   );
 };
