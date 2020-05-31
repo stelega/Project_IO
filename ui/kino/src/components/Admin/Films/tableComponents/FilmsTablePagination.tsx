502e9be46fb454465f35c87570745b8e5180e354
Piotr Ksel <piotrek_ksel@wp.pl>
2020-05-31 04:18:06 +0200

    dodanie zmiany rekordów na stronie, zmiany strony i sortowanina

--- ui/kino/src/components/Admin/Films/tableComponents/FilmsTablePagination.tsx
+++ ui/kino/src/components/Admin/Films/tableComponents/FilmsTablePagination.tsx
@@ -23,8 +23,10 @@ const FilmsTablePagination = (props: FilmsTablePaginationProps) => {
       component='div'
       count={totalCount}
       rowsPerPage={rowsPerPage}
-      page={page}
-      onChangePage={onChangePage}
+      page={page - 1}
+      onChangePage={(event: unknown, pageNumber: number) =>
+        onChangePage(event, pageNumber + 1)
+      }
       onChangeRowsPerPage={onChangeRowsPerPage}
       labelRowsPerPage={'Wierszy na stronę'}
       labelDisplayedRows={({ from, to, count }) => `${from}-${to} z ${count}`}
--- ui/kino/src/services/FilmService.ts
+++ ui/kino/src/services/FilmService.ts
@@ -2,7 +2,17 @@ import { apiGetFilms } from './../api/filmsApi';
 import { PagedList } from './../models/PagedList';
 import { Film } from '../models/Film';
 
-export const getFilms = async (): Promise<PagedList<Film>> => {
-  const films: PagedList<Film> = await apiGetFilms();
+export const getFilms = async (
+  rowsPerPage?: number,
+  page?: number,
+  orderBy?: string,
+  order?: 'asc' | 'desc'
+): Promise<PagedList<Film>> => {
+  const films: PagedList<Film> = await apiGetFilms(
+    rowsPerPage,
+    page,
+    orderBy,
+    order
+  );
   return films;
 };
